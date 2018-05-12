a816bfb51811635c16b8846e321fa8659d7e6f9c
DaltonHart <daltonvaren@gmail.com>
2018-05-12 16:43:52 -0700

    started function in js

---
+++ project-code/main.js
@@ -0,0 +1,11 @@
+// create all element variables
+
+
+
+function change() { 
+    console.log('clicked!');
+    };
+
+var button1 = document.querySelector('lacroix');
+
+button1.addEventListener('click', change);
