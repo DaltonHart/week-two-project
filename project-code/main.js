382907dac1dca22bd10fadea0df6de922f647a47
DaltonHart <daltonvaren@gmail.com>
2018-05-13 13:38:36 -0700

    updated files and defined basic variables in js

--- project-code/main.js
+++ project-code/main.js
@@ -1,11 +1,71 @@
-// create all element variables
+// create img element variables (will turn into array later)
 
+var item1Img = document.getElementById('item1Img')
+var item2Img = document.getElementById('item2Img')
+var item3Img = document.getElementById('item3Img')
+var item4Img = document.getElementById('item4Img')
+var item5Img = document.getElementById('item5Img')
+var item6Img = document.getElementById('item6Img')
 
+// create item button variables
+var item1but = document.getElementById('item1but')
+var item2but = document.getElementById('item2but')
+var item3but = document.getElementById('item3but')
+var item4but = document.getElementById('item4but')
+var item5but = document.getElementById('item5but')
+var item6but = document.getElementById('item6but')
 
-function change() { 
-    console.log('clicked!');
-    };
+// set up catagory variables 
+var lacroixCat = document.getElementById('lacroix')
+var vegtableCat = document.getElementById('vegtables')
+var fruitCat = document.getElementById('fruit')
+//create event listeners for categories 
+lacroixCat.addEventListener('click',changeCat1);
+vegtableCat.addEventListener('click',changeCat2);
+fruitCat.addEventListener('click', changeCat3);
 
-var button1 = document.querySelector('lacroix');
+// create functions to change all element imgs to corresponding category
+function changeCat1 (){
+    item1Img.src = 'assets/grapefruitLAC.jpeg'
+    item2Img.src = 'assets/coconutLAC.jpeg'
+    item3Img.src = 'assets/kiwiLAC.jpeg'
+    item4Img.src = 'assets/mangoLAC.jpeg'
+    item5Img.src = 'assets/passionLAC.jpeg'
+    item6Img.src = 'assets/tangerineLAC.jpeg'
+    item1but.innerText = 'Grape Fruit La Croix'
+    item2but.innerText = 'Coconut La Croix'
+    item3but.innerText = 'Kiwi Sandia La Croix'
+    item4but.innerText = 'Mango La Croix'
+    item5but.innerText = 'Passion Fruit La Croix'
+    item6but.innerHTML = 'Tangerine La Croix'
+}
 
-button1.addEventListener('click', change);
+function changeCat2 (){
+    item1Img.src = 'assets/bellpepper.jpeg'
+    item2Img.src = 'assets/lettuce.jpeg'
+    item3Img.src = 'assets/onion.jpeg'
+    item4Img.src = 'assets/spinach.jpeg'
+    item5Img.src = 'assets/tomato.jpeg'
+    item6Img.src = 'assets/broccoli.jpeg'
+    item1but.innerText = 'Bellpepper'
+    item2but.innerText = 'Lettuce'
+    item3but.innerText = 'Onion'
+    item4but.innerText = 'Spinach'
+    item5but.innerText = 'Tomato'
+    item6but.innerHTML = 'Broccoli'
+}
+
+function changeCat3 (){
+    item1Img.src = 'assets/cuties.jpeg'
+    item2Img.src = 'assets/grapes.jpeg'
+    item3Img.src = 'assets/strawberry.jpeg'
+    item4Img.src = 'assets/banana.jpeg'
+    item5Img.src = 'assets/rasberry.jpeg'
+    item6Img.src = 'assets/blackberry.jpeg'
+    item1but.innerText = 'Cuties'
+    item2but.innerText = 'Grapes'
+    item3but.innerText = 'Strawberries'
+    item4but.innerText = 'Bananas'
+    item5but.innerText = 'Rasberries'
+    item6but.innerHTML = 'Blackberries'
+}
